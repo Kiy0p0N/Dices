de9cac34b3297afc1d1e66ac7dc0bd8fe46de134
Felipe <135260228+Kiy0p0N@users.noreply.github.com>
2024-03-19 14:43:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var aleatoryNumber1 = Math.floor(Math.random()*6)+1;
-var faceDice1 = "images/dice" + aleatoryNumber1 + ".png";
-var dado1 = document.querySelector(".img1");
-dado1.setAttribute("src", faceDice1);
+document.querySelector("button").addEventListener("click", function (){
+    
+    var number1 = Math.floor(Math.random()*6)+1; //generate a number 1-6
+    var aleatoryDice1 = "images/dice" + number1 + ".png";
+    var dice1 = document.querySelector(".img1"); //select class img1
+    dice1.setAttribute("src", aleatoryDice1); //to change src for img
 
-var aleatoryNumber2 = Math.floor(Math.random()*6)+1;
-var faceDice2 = "images/dice" + aleatoryNumber2 + ".png";
-var dado2 = document.querySelector(".img2");
-dado2.setAttribute("src", faceDice2);
+    var number2 = Math.floor(Math.random()*6)+1;
+    var aleatoryDice2 = "images/dice" + number2 + ".png";
+    var dice2 = document.querySelector(".img2");
+    dice2.setAttribute("src", aleatoryDice2);
 
-if (aleatoryNumber1 > aleatoryNumber2) {
-    document.querySelector("h1").innerHTML = "PLAYER 1 WIN!!!";
-}
-else if (aleatoryNumber1 < aleatoryNumber2) {
-    document.querySelector("h1").innerHTML = "PLAYER 2 WIN!!!";
-} else {
-    document.querySelector("h1").innerHTML = "EMPATE!";
-}
+    if (number1 > number2){
+        document.querySelector(".result").innerHTML = "🚩Player 1 is the WINNER!";
+    }
+    else if (number1 < number2){
+        document.querySelector(".result").innerHTML = "Player 2 is the WINNER!🚩";
+    } else {
+        document.querySelector(".result").innerHTML = "🚩DRAWN!🚩";
+    }
+})
